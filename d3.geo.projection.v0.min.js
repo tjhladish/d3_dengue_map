389fe23a0c608e607106637cf2f8c813b77d1d47
Carl Pearson <cap10@ufl.edu>
2015-05-26 11:16:34 -0400

    all necessary items for localhost serving

---
+++ d3.geo.projection.v0.min.js
@@ -0,0 +1,8 @@
+HTTP/1.0 302 Found
+Location: /cgi-bin/ipdiags.ha
+Pragma: no-cache
+Content-Type: text/html
+
+<html><meta http-equiv=Refresh content=0;url=/cgi-bin/ipdiags.ha>
+<body></body></html>
+
